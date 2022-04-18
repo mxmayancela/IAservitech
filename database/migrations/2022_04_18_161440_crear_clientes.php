39bb887caebd90470f731c1673cd83001c25044f
mxmayancela <mxmayancela@gmail.com>
2022-04-18 12:09:07 -0500

    Creacion de migraciones cliente y paquetes

---
+++ database/migrations/2022_04_18_161440_crear_clientes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',60);
+            $table->string('apellido',60);
+            $table->string('cedula',10);
+            $table->string('telefono',10);
+            $table->string('direccion',120);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
